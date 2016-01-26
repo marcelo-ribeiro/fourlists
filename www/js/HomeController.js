4387d8735baaa6c9174dfa2c9b97c080c182e75b
marcelo-ribeiro <marceloribeiromidia@gmail.com>
2016-01-26 20:21:57 -0300

    Improvements

--- www/js/HomeController.js
+++ www/js/HomeController.js
@@ -30,7 +30,7 @@
       vm.tasks = TaskFactory.getAll();
       vm.tasks.$loaded()
       .then(function(data) {
-
+        console.log('tasks:', data)
       })
       .catch(function(error) {
         console.log("Error:", error);
@@ -43,28 +43,6 @@
     vm.openTaskModal = function (item) {
       $state.go('editTask',{taskId: item.$id});
     }
-
-
-    vm.openList = function( list, focus ) {
-      // if ( list.opened )
-      //   return false;
-
-      // list.opened = true;
-
-      // vm.listId = list.id;
-      // console.log(list);
-
-      // $state.go( '.list', { listId: list.id } );
-    };
-
-    vm.closeList = function( list ) {
-      // if ( !list.opened )
-      //   return false;
-
-      // list.opened = false;
-
-      $state.go('^');
-    };
 
 
     // Private functions
